99a66fde082441b9843953611493b37c0d8042a6
PAKhoi <khoiphamvk123@gmail.com>
2026-03-11 20:06:21 +0700

    Route structure

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,9 +1,10 @@
-const express = require("express");
-
-const userRoutes = require("../modules/user/user.route");
+import express from "express";
+import userRoutes from "../modules/user/user.route.js";
+import roleRoutes from "../modules/role/role.route.js";
 
 const router = express.Router();
 
 router.use("/users", userRoutes);
+router.use("/roles", roleRoutes);
 
-module.exports = router;
+export default router;
