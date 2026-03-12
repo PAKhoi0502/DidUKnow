dd7f436ee95e1ecb9d84542c602603d223eab764
PAKhoi <khoiphamvk123@gmail.com>
2026-03-12 23:04:29 +0700

    Mount route

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,10 +1,18 @@
 import express from "express";
 import userRoutes from "../modules/user/user.route.js";
 import roleRoutes from "../modules/role/role.route.js";
+import factRoutes from "../modules/fact/fact.route.js";
+import categoryRoutes from "../modules/category/category.route.js";
+import languageRoutes from "../modules/language/language.route.js";
+import mediaRoutes from "../modules/media/media.route.js";
 
 const router = express.Router();
 
 router.use("/users", userRoutes);
 router.use("/roles", roleRoutes);
+router.use("/facts", factRoutes);
+router.use("/categories", categoryRoutes);
+router.use("/languages", languageRoutes);
+router.use("/media", mediaRoutes);
 
 export default router;
